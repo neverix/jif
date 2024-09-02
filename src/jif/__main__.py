90fd37fec2b0688b2c25eee47b851f5a822ba766
nev <sshabalin3@gatech.edu>
2024-09-02 02:43:24 +0000

    LR tuning?..

--- src/jif/__main__.py
+++ src/jif/__main__.py
@@ -11,7 +11,7 @@ from jax import sharding
 from penzai import pz
 from penzai.models.transformer import model_parts
 from penzai.toolshed import basic_training, sharding_util
-from tqdm import trange
+from tqdm.auto import tqdm, trange
 
 import wandb
 
@@ -29,12 +29,12 @@ def clone_schedule_free(optimizer):
     return optax.GradientTransformation(init, optimizer.update)
 
 
-def main(
+def train(
     batch_size=256,
     seq_len=128,
     diffusion_eps = 1e-3,
     ema_decay=0.995,
-    n_steps=100_000,
+    n_steps=10_000,
     lr=1e-3,
     schedule_free=False,
     b1=0.9,
@@ -48,13 +48,13 @@ def main(
     accurate_flops_calc=False,
     profile=False,
     size="small",
+    quiet=False,
 ):
+    profile = profile and not quiet
+    
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-
-    run = wandb.init(project="jif")
-    wandb_config = run.config
 
     data_generator, detokenize, n_classes, bos_token = get_data(batch_size, seq_len)
 
@@ -82,34 +82,39 @@ def main(
                        n_layers=n_layers, d_model=d_model, n_kv_heads=d_model//64, q_rep=1, qk_dim=64, v_dim=64,
                        d_ff=d_model * 3)
 
-    wandb_config.size = size
-    wandb_config.n_layers = n_layers
-    wandb_config.d_model = d_model
-    wandb_config.profile = profile
-    wandb_config.seed = seed
-    wandb_config.accurate_flops_calc = accurate_flops_calc
-    wandb_config.grad_clip_norm = grad_clip_norm
-    wandb_config.warmup_steps = warmup_steps
-    wandb_config.n_classes = n_classes
-    wandb_config.sample_steps = sample_steps
-    wandb_config.bos_token = bos_token
-    wandb_config.n_steps = n_steps
-    wandb_config.lr = lr
-    wandb_config.diffusion_eps = diffusion_eps
-    wandb_config.ema_decay = ema_decay
-    wandb_config.batch_size = batch_size
-    wandb_config.seq_len = seq_len
-    wandb_config.schedule_free = schedule_free
-    wandb_config.b1 = b1
-    wandb_config.b2 = b2
-    wandb_config.grad_clip_norm = grad_clip_norm
-    wandb_config.wandb_every = wandb_every
-    wandb_config.sample_every = sample_every
-    wandb_config.dp = mesh.shape["dp"]
-    wandb_config.mp = mesh.shape["mp"]
-    wandb_config.ema_dtype = ema_dtype
-    for k, v in config.__dict__.items():
-        setattr(wandb_config, k, v)
+    if not quiet:
+        run = wandb.init(project="jif")
+        wandb_config = run.config
+
+        wandb_config.size = size
+        wandb_config.n_layers = n_layers
+        wandb_config.d_model = d_model
+        wandb_config.profile = profile
+        wandb_config.seed = seed
+        wandb_config.accurate_flops_calc = accurate_flops_calc
+        wandb_config.grad_clip_norm = grad_clip_norm
+        wandb_config.warmup_steps = warmup_steps
+        wandb_config.n_classes = n_classes
+        wandb_config.sample_steps = sample_steps
+        wandb_config.bos_token = bos_token
+        wandb_config.n_steps = n_steps
+        wandb_config.lr = lr
+        wandb_config.diffusion_eps = diffusion_eps
+        wandb_config.ema_decay = ema_decay
+        wandb_config.batch_size = batch_size
+        wandb_config.seq_len = seq_len
+        wandb_config.schedule_free = schedule_free
+        wandb_config.b1 = b1
+        wandb_config.b2 = b2
+        wandb_config.grad_clip_norm = grad_clip_norm
+        wandb_config.wandb_every = wandb_every
+        wandb_config.sample_every = sample_every
+        wandb_config.dp = mesh.shape["dp"]
+        wandb_config.mp = mesh.shape["mp"]
+        wandb_config.ema_dtype = ema_dtype
+        for k, v in config.__dict__.items():
+            setattr(wandb_config, "model." + k, v)
+
     ema_dtype = getattr(jnp, ema_dtype)
 
     key = jax.random.key(seed)
@@ -120,7 +125,8 @@ def main(
                                        mesh=mesh,
                                        axis_name_to_mesh_name=axis_name_to_mesh_name)
     param_count = sum(v.value.data_array.size for v in pz.unbind_params(model)[1])
-    print(f"Parameter count: {param_count}")
+    if not quiet:
+        print(f"Parameter count: {param_count}")
     model = (model.select().at_instances_of(pz.nn.Residual)
              .insert_after(sharding_util.ConstrainShardingByName(
                  mesh, axis_name_to_mesh_name=axis_name_to_mesh_name)))
@@ -193,40 +199,74 @@ def main(
         return get_loss(model, rng, state, sample, update_state=update_state)[0]
 
     model_flops = None
+    losses = []
+    log_dict = {}
     for step, sample in zip((bar := trange(n_steps)), data_generator()):
         sample = jax.device_put(jnp.asarray(sample.numpy().astype(np.uint32), device=jax.devices("cpu")[0]), data_sharding)
-        if model_flops is None:
-            if accurate_flops_calc:
-                model_variables = pz.unbind_variables(trainer.model)[1]
-                loss_compiled = get_loss_grad.lower([v.freeze() for v in model_variables],
-                                            jax.random.key(0), None,
-                                            sample=sample, update_state=False).compile()
-                model_flops = loss_compiled.cost_analysis()[0]["flops"]
-                del model, model_variables
-            else:
-                model_flops = batch_size * seq_len * 6 * param_count
-            print("Model FLOPs:", model_flops)
-            print(f" (per token: {model_flops / seq_len / batch_size:.2f})")
+        if not quiet:
+            if model_flops is None:
+                if accurate_flops_calc:
+                    model_variables = pz.unbind_variables(trainer.model)[1]
+                    loss_compiled = get_loss_grad.lower([v.freeze() for v in model_variables],
+                                                jax.random.key(0), None,
+                                                sample=sample, update_state=False).compile()
+                    model_flops = loss_compiled.cost_analysis()[0]["flops"]
+                    del model, model_variables
+                else:
+                    model_flops = batch_size * seq_len * 6 * param_count
+                print("Model FLOPs:", model_flops)
+                print(f" (per token: {model_flops / seq_len / batch_size:.2f})")
         if step == 5 and profile:
             jax.profiler.start_trace("/tmp/tensorboard")
         out = trainer.step(sample=sample)
+        losses.append(out["loss"])
         if out.get("err") is not None:
             out["err"].throw()
         if step == 25 and profile:
             jax.profiler.stop_trace()
+            
+        log_dict = dict(loss=out["loss"], loss_sma=np.mean(losses[-100:]))
+        itps = bar.format_dict["rate"]
+        if itps is not None and model_flops is not None:
+            one_v4_chip_flops = 275 * 1e12  # https://cloud.google.com/tpu/docs/v4
+            total_flops = one_v4_chip_flops * len(jax.devices("tpu"))
+            log_dict["mfu"] = (itps * model_flops) / total_flops
+        bar.set_postfix(**log_dict)
         if step % wandb_every == 0:
-            bar.set_postfix(loss=out["loss"])
-            log_dict = dict(loss=out["loss"])
-            itps = bar.format_dict["rate"]
-            if itps is not None:
-                one_v4_chip_flops = 275 * 1e12  # https://cloud.google.com/tpu/docs/v4
-                total_flops = one_v4_chip_flops * len(jax.devices("tpu"))
-                log_dict["mfu"] = (itps * model_flops) / total_flops
-            wandb.log(log_dict, step=step)
-        if step % sample_every == 0:
-            print(f"Sampling at step {step}...")
-            print(detokenize(get_samples(trainer, 4, seq_len, jax.random.fold_in(sample_key, step)).tolist()))
-        
+            if not quiet:
+                wandb.log(log_dict, step=step)
+        if not quiet:
+            if step % sample_every == 0:
+                print(f"Sampling at step {step}...")
+                print(detokenize(get_samples(trainer, 4, seq_len, jax.random.fold_in(sample_key, step)).tolist()))
+    return log_dict
+
+
+def main(*args, **kwargs):
+    from matplotlib import pyplot as plt
+    from collections import defaultdict
+    from itertools import product
+    lrs = np.linspace(1e-5, 2e-3, 10).tolist()
+    models = ["small", "medium", "big"]
+    colors = ["r", "g", "b"]
+    lrs_sampled = defaultdict(list)
+    losses = defaultdict(list)
+    all_configs = list(product(lrs, models))
+    random.Random(0).shuffle(all_configs)
+    for lr, model in tqdm(all_configs):
+        print("Training", model, "model with", lr, "learning rate")
+        lrs_sampled[model].append(lr)
+        log_dict = train(*args, **kwargs | {"n_steps": 2_000, "lr": lr, "size": "small"},
+                         quiet=True, profile=False)
+        losses[model].append(log_dict["loss"])
+        for model, color in zip(models, colors):
+            plt.plot(lrs_sampled[model], losses[model], label=model, c=color)
+            plt.scatter(lrs_sampled[model], losses[model], label=model, marker="x", c=color)
+        plt.xlabel("Learning rate")
+        plt.ylabel("Final loss")
+        plt.legend()
+        plt.savefig("lr_search.png")
+        plt.close()
 
 
 if __name__ == "__main__":
