93b30ac8e121599d25570eec47409018166d2234
nev <nev@neverix.io>
2025-07-25 06:34:49 +0000

    Change file structure, update to uv

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # jif
+
+```bash
+uv sync --extra vis  # install dependencies
+uv run jif  # run training sweep
+```
--- jif/__main__.py
+++ jif/__main__.py
@@ -1,6 +1,7 @@
 import random
 from functools import partial
 
+import fire
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -252,39 +253,5 @@ def train(
     return log_dict
 
 
-def sweep(*args, min_lr, max_lr, out_name, **kwargs):
-    from matplotlib import pyplot as plt
-    from collections import defaultdict
-    from itertools import product
-    lrs = np.exp(np.linspace(np.log(min_lr), np.log(max_lr), 10))
-    models = ["small", "medium", "big"]
-    colors = ["r", "g", "b"]
-    lrs_sampled = defaultdict(list)
-    losses = defaultdict(list)
-    all_configs = list(product(lrs, models))
-    # random.Random(0).shuffle(all_configs)
-    random.shuffle(all_configs)
-    for lr, model in tqdm(all_configs):
-        print("Training", model, "model with", lr, "learning rate")
-        lrs_sampled[model].append(lr)
-        log_dict = train(*args, **kwargs | {"n_steps": 1_000, "lr": lr, "size": model},
-                         quiet=True, profile=False)
-        losses[model].append(log_dict["loss_sma"])
-
-        for model, color in zip(models, colors):
-            plt.scatter(lrs_sampled[model], losses[model], label=model, c=color)
-            if losses[model]:
-                lr, loss = zip(*sorted(zip(lrs_sampled[model], losses[model]), key=lambda x: x[0]))
-                plt.plot(lr, loss, c=color)
-        plt.xlim(lrs[0], lrs[-1])
-        plt.xscale("log")
-        plt.xlabel("Learning rate")
-        plt.ylabel("Final loss")
-        plt.legend()
-        plt.savefig(f"{out_name}.png")
-        plt.close()
-
-
 if __name__ == "__main__":
-    # sweep(min_lr=7e-4, max_lr=3e-3, out_name="lr_batch_search")
-    sweep(min_lr=5e-4, max_lr=5e-3, out_name="lr_search", fix_batch_size=True)
+    fire.Fire(train)
